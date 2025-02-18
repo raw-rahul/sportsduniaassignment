3043b4341c90b453f4588ea4241b98ef369abf19
Rahul Kumar <rahuleigle@gmail.com>
2025-02-19 00:46:20 +0530

    changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_API_KEY;
-const BASE_URL = "https://newsapi.org/v2/everything";
+const BASE_URL = 'https://newsapi.org/v2/everything';
 
-export const fetchNews = async (query = "react") => {
-  const response = await axios.get(BASE_URL, {
-    params: {
-      q: query,
-      apiKey: API_KEY,
-    },
-  });
-  return response.data.articles;
+export const fetchNews = async (query = 'react') => {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        q: query,
+        apiKey: API_KEY,
+      },
+    });
+    return response.data.articles;
+  } catch (error) {
+    console.error('Error fetching news:', error);
+    return [];
+  }
 };
